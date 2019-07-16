a0959348baba854e10b6596e836065e7b5383e86
theAlanDu <theAlanDu@users.noreply.github.com>
2019-07-15 17:37:54 -0700

    Minor Changes

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,20 @@
-function test()
+function notes_section()
 {
-    alert("You have clicked the test button");
-    document.getElementById("added-section").innerHTML += "Hello World";
+    document.getElementById("notes-section").style.display = "block";
+    document.getElementById("tennis-section").style.display = "none";
+    document.getElementById("stories-section").style.display = "none";
+}
+
+function tennis_section()
+{
+    document.getElementById("notes-section").style.display = "none";
+    document.getElementById("tennis-section").style.display = "block";
+    document.getElementById("stories-section").style.display = "none";
+}
+
+function stories_section()
+{
+    document.getElementById("notes-section").style.display = "none";
+    document.getElementById("tennis-section").style.display = "none";
+    document.getElementById("stories-section").style.display = "block";
 }
