564dfb41bd07c5fb0f16950c8ede8dd12bbbee04
theAlanDu <theAlanDu@users.noreply.github.com>
2019-08-09 22:07:28 -0700

    Nav Menu Update

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,7 @@
+function display(id,value)
+{
+    document.getElementById(id).style.display = value;
+}
 function notes_section()
 {
     document.getElementById("notes-section").style.display = "block";
@@ -17,4 +21,24 @@ function stories_section()
     document.getElementById("notes-section").style.display = "none";
     document.getElementById("tennis-section").style.display = "none";
     document.getElementById("stories-section").style.display = "block";
+}
+
+function nav_toggle()
+{
+    display("nav_toggle","none");
+    display("nav_exit","block");
+    setTimeout(display,50,"shade","flex");
+    setTimeout(display,100,"link_1","block");
+    setTimeout(display,150,"link_2","block");
+    document.getElementsByClassName("footer").style.position = "absolute";
+    document.getElementsByClassName("footer").style.bottom = "0";
+}
+function nav_exit()
+{
+    display("nav_toggle","block");
+    display("nav_exit","none");
+    setTimeout(display,50,"link_2","none")
+    setTimeout(display,100,"link_1","none");
+    setTimeout(display,150,"shade","none");
+
 }
